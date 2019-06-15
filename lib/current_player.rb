82f01441551838bac8b92050ee5c208171793892
Morgan Stanley <morganpstanley@gmail.com>
2019-06-15 23:35:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,14 @@ def turn_count(board)
   return i
 end
 
-def current_player(i)
-  if i % 2 == 0
-    current_player = "O"
-  else
-    current_player = "X"
+def current_player(board)
+  i = 0
+  board.each do |p_symbol|
+    if p_symbol == "X" || p_symbol == "O"
+      i += 1
+    else
+      i += 0
+    end
   end
+  return i
 end
